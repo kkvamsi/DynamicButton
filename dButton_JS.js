62f02267b95b66b18dc19f12e6b7dc4919efbdf7
Krishna Vamsi <ismavdon@gmail.com>
2015-05-06 17:43:36 +0530

    Update dButton_JS.js

--- dButton_JS.js
+++ dButton_JS.js
@@ -8,7 +8,7 @@ alert("Hello");
 $(document).ready(function(){
 
 $('#testButton').html('jquery working');
-$('#addButton').click(function(){$('#addButton').clone(true).insertAfter(this);});
+$('#addButton').click(function(){$('#parentDiv').clone(true).insertAfter(this);});
 //$('#removeButton').click(function(){$('body').last().remove();});
 
 }
